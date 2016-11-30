dab7a34947fc70d074d71c76000a4f1ff409517c
Rohitash Tulyani <rtulyani@sapient.com>
2016-11-30 17:11:20 +0530

    RED - first test case : test_zero_item_in_cart

---
+++ test/com/cleancode/checkout/CheckOutCalculatorTest.java
@@ -0,0 +1,19 @@
+package com.cleancode.checkout;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by rtulya on 11/30/2016.
+ */
+public class CheckOutCalculatorTest {
+    @Test
+    public void test_zero_item_in_cart() {
+        CheckOutCalculator checkOutCalculator =  new CheckOutCalculator();
+        List<String> itemsInCart = Arrays.asList();
+        Assert.assertEquals(0, checkOutCalculator.calculatePrice());
+    }
+}
