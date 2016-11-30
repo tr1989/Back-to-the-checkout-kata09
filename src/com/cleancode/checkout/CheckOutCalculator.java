30d77db2edee99b72cea787091456372858b713e
Rohitash Tulyani <rtulyani@sapient.com>
2016-11-30 18:14:45 +0530

    REFACTOR - Added method getPriceOfItem() for getting price from hashmap based on item

--- src/com/cleancode/checkout/CheckOutCalculator.java
+++ src/com/cleancode/checkout/CheckOutCalculator.java
@@ -1,17 +1,31 @@
 package com.cleancode.checkout;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by rtulya on 11/30/2016.
  */
 public class CheckOutCalculator {
 
+    private Map<String, Integer> priceList = new HashMap<String, Integer>();
+
+    public CheckOutCalculator() {
+        this.priceList.put("A", 10);
+        this.priceList.put("B", 15);
+    }
+
     public int calculatePrice(List<String> itemsInCart){
         if(itemsInCart.contains("A")){
-            return 10;
+            return getPriceOfItem("A");
         } else if (itemsInCart.contains("B")){
-            return 15;
+            return getPriceOfItem("B");
+        }
+        return getPriceOfItem("");
+    }
+
+    protected int getPriceOfItem(String itemName){
+        if(priceList.containsKey(itemName)) {
+            return priceList.get(itemName);
         }
         return 0;
     }
