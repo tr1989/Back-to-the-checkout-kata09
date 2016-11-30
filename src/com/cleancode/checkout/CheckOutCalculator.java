a59ce34c02df98b4429d57a25902522e01cfedbb
Rohitash Tulyani <rtulyani@sapient.com>
2016-11-30 17:35:07 +0530

    GREEN - Implemented calculatePrice for zero items

---
+++ src/com/cleancode/checkout/CheckOutCalculator.java
@@ -0,0 +1,13 @@
+package com.cleancode.checkout;
+
+import java.util.List;
+
+/**
+ * Created by rtulya on 11/30/2016.
+ */
+public class CheckOutCalculator {
+
+    public int calculatePrice(List<String> itemsInCart){
+        return 0;
+    }
+}
